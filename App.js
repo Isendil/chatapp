90db322e88b6e72c2b2eec45fd032beeb0c4cfe2
Dimitrios Ellinidis <isendil_1@yahoo.com>
2020-06-30 18:15:31 -0400

    commit new changes for 5.4

--- App.js
+++ App.js
@@ -1,31 +1,25 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Start from "./components/Start";
+
+//Import screens
 import Chat from "./components/Chat";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import Start from "./components/Start";
 
-const firebase = require("firebase");
-require("firebase/firestore");
-const Stack = createStackNavigator();
+//Import navigation
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Start">
-        <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="Chat" component={Chat} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+const navigator = createStackNavigator({
+  Start: {
+    screen: Start,
+    navigationOptions: {
+      //hide navigation bar on Start screen
+      headerShown: false,
+    },
   },
+  Chat: { screen: Chat },
 });
+
+const navigatorContainer = createAppContainer(navigator);
+
+export default navigatorContainer;
--- components/Chat.js
+++ components/Chat.js
@@ -1,33 +1,145 @@
 import React, { Component } from "react";
 import {
   StyleSheet,
-  View,
-  TextInput,
   Text,
+  View,
   Button,
-  TouchableOpacity,
+  TextInput,
+  ImageBackground,
+  Platform,
 } from "react-native";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
 import KeyboardSpacer from "react-native-keyboard-spacer";
 
+const firebase = require("firebase");
+require("firebase/firestore");
+
 export default class Chat extends Component {
-  constructor() {
-    super();
-    if (!firebase.apps.length) {
-      firebase.initializeApp({
-        apiKey: "AIzaSyCm_Yc6MZUjuW3q7PQMmsJXWkIg4Eup-_g",
-        authDomain: "test-95ec6.firebaseapp.com",
-        databaseURL: "https://test-95ec6.firebaseio.com",
-        projectId: "test-95ec6",
-        storageBucket: "test-95ec6.appspot.com",
-        messagingSenderId: "111540181902",
-        appId: "1:111540181902:web:994bd356ac27921517e2df",
-        measurementId: "G-22NLXCDQ6Z",
-      });
-    }
-    this.state = { text: "" };
+  //pulling in information from Start.js name/color
+  static navigationOptions = ({ navigation }) => {
+    return {
+      name: navigation.state.params.name,
+    };
+  };
+
+  constructor(props) {
+    super(props);
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyCm_Yc6MZUjuW3q7PQMmsJXWkIg4Eup-_g",
+      authDomain: "test-95ec6.firebaseapp.com",
+      databaseURL: "https://test-95ec6.firebaseio.com",
+      projectId: "test-95ec6",
+      storageBucket: "test-95ec6.appspot.com",
+      messagingSenderId: "111540181902",
+      appId: "1:111540181902:web:994bd356ac27921517e2df",
+      measurementId: "G-22NLXCDQ6Z",
+    });
+    this.referenceMessageUser = null;
+    this.referenceMessages = firebase.firestore().collection("messages");
+    this.state = {
+      messages: [],
+      user: {
+        _id: "",
+        name: "",
+        avatar: "",
+      },
+      uid: 0,
+    };
   }
+
+  // Change bubble colour
+  renderBubble(props) {
+    {
+      /* Colour options
+      '#e67e22', // carrot
+      '#2ecc71', // emerald
+      '#3498db', // peter river
+      '#8e44ad', // wisteria
+      '#e74c3c', // alizarin
+      '#1abc9c', // turquoise
+      '#2c3e50', // midnight blue
+       https://coolors.co/ */
+    }
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: "#8e44ad",
+          },
+          right: {
+            backgroundColor: "#2ecc71",
+          },
+        }}
+      />
+    );
+  }
+
+  // Set user ID, name and avatar
+  setUser = (_id, name = "anonymous") => {
+    this.setState({
+      user: {
+        _id: _id,
+        name: name,
+        avatar: "https://placeimg.com/140/140/any",
+      },
+    });
+  };
+
+  get user() {
+    return {
+      name: this.props.navigation.state.params.name,
+      _id: this.state.uid,
+      id: this.state.uid,
+    };
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    //go through each document
+    querySnapshot.forEach((doc) => {
+      var data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+        // messages: data.message,
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
+
+  //adding messages to the database and setting the state of user id
   componentDidMount() {
+    // this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+      if (!user) {
+        await firebase.auth().signInAnonymously();
+      }
+      //update user state with currently active user data
+      if (user) {
+        //do something
+        //now you are sure that tthere is a user in the if statement
+      }
+      this.setState({
+        uid: user.uid,
+        loggedInText: "Welcome to Mad Chatter",
+      });
+      if (user.uid) {
+        // Do something
+        console.log(user.uid);
+      }
+      // Will not result in an error
+      this.referenceMessageUser = firebase.firestore().collection("messages");
+
+      this.unsubscribeMessageUser = this.referenceMessageUser.onSnapshot(
+        this.onCollectionUpdate
+      );
+    });
     this.setState({
       messages: [
         {
@@ -42,46 +154,60 @@ export default class Chat extends Component {
         },
         {
           _id: 2,
-          text: "This is a system message",
+          text:
+            this.props.navigation.state.params.name + " has entered the chat",
           createdAt: new Date(),
           system: true,
         },
       ],
     });
   }
-  onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
+
+  //unmounting
+  componentWillUnmount() {
+    this.authUnsubscribe();
+    this.unsubscribeMessageUser();
   }
-  renderBubble(props) {
-    return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          right: {
-            backgroundColor: "blue",
-          },
-        }}
-      />
+
+  //Adding messages to the database
+  addMessage() {
+    const message = this.state.messages[0];
+    this.referenceMessages.add({
+      _id: this.state.messages[0]._id,
+      text: this.state.messages[0].text || "",
+      createdAt: this.state.messages[0].createdAt,
+      user: this.state.messages[0].user,
+    });
+  }
+
+  // Send message function
+  onSend(messages = []) {
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => this.addMessage()
     );
   }
 
   render() {
-    let name = this.props.route.params.name;
-
-    this.props.navigation.setOptions({ title: name });
     return (
-      <View style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          backgroundColor: this.props.navigation.state.params.color,
+        }}
+      >
+        <Text> Hello {this.props.navigation.state.params.name}</Text>
         <GiftedChat
-          renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           onSend={(messages) => this.onSend(messages)}
-          user={{
-            _id: 1,
-          }}
+          renderBubble={this.renderBubble.bind(this)}
+          user={this.state.user}
         />
-        {/* {Platform.OS === "android" ? <KeyboardSpacer /> : null} */}
+        {/* Keyboard spacer for android only. */}
+        {Platform.OS === "android" ? <KeyboardSpacer /> : null}
       </View>
     );
   }
@@ -89,9 +215,8 @@ export default class Chat extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1, // Sets the width and height of the device
+    color: "#FFFFFF",
+    backgroundColor: "#000000",
   },
 });
